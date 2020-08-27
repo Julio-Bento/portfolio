e61d7b7d050c9689a07b021ca26049f47f52ea0b
Tribouillier <julien.tribouillier@gmail.com>
2020-08-27 18:16:42 +0200

    add scrollreveal and some animation to the text

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+const sr = ScrollReveal();
+
+sr.reveal('.neon');
+sr.reveal('.jumbotron');
+sr.reveal('.lead.text-center',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '200px',
+});
+sr.reveal('.js1', {
+    duration: 2000,
+    origin: 'left',
+    distance: '200px',
+});
+sr.reveal('.js2', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '200px',
+});
+sr.reveal('.js3', {
+    duration: 2000,
+    origin: 'right',
+    distance: '200px',
+});
+sr.reveal('.jumbotron');
