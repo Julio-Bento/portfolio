144ac4b44489c52f212340d62a77c0d37826d5dc
Tribouillier <julien.tribouillier@gmail.com>
2020-08-27 18:44:27 +0200

    add reset to animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,20 +6,24 @@ sr.reveal('.lead.text-center',{
     duration: 2000,
     origin: 'bottom',
     distance: '200px',
+    reset: 'true',
 });
 sr.reveal('.js1', {
     duration: 2000,
     origin: 'left',
     distance: '200px',
+    reset: 'true',
 });
 sr.reveal('.js2', {
     duration: 2000,
     origin: 'bottom',
     distance: '200px',
+    reset: 'true',
 });
 sr.reveal('.js3', {
     duration: 2000,
     origin: 'right',
     distance: '200px',
+    reset: 'true',
 });
 sr.reveal('.jumbotron');
