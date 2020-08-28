c3040f8d4ccf98c172fcd0944230c94762d10238
Bento <oobento01oo@gmail.com>
2020-08-28 09:32:19 +0200

    style: fix navbartop + link navbar items to the different sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,40 @@
-const sr = ScrollReveal({
-    duration: 2000,
-    distance: '120px',
-    reset: 'true',
-});
-
-sr.reveal('.neon',{
-    origin: 'left',
-    opacity: null,
-});
-sr.reveal('.jumbotron',{
-
-});
-sr.reveal('.lead.text-center');
-
-sr.reveal('.js1',{
-    origin: 'left',
-});
-sr.reveal('.js2',{
-    origin: 'bottom',
-});
-sr.reveal('.js3',{
-    origin: 'right',
-});
-sr.reveal('.title');
-sr.reveal('#projets',{
-    origin: 'left',
-    delay: 300,
-});
-sr.reveal('.row.align-items-center',{
-    delay: 200
-})
-sr.reveal('.bento',{
-    delay: 150,
-    origin: 'right'
-})
-sr.reveal('.julio',{
-    delay: 150,
-    origin: 'left'
-})
+// const sr = ScrollReveal({
+//     duration: 2000,
+//     distance: '120px',
+//     reset: 'true',
+// });
+//
+// sr.reveal('.neon',{
+//     origin: 'left',
+//     opacity: null,
+// });
+// sr.reveal('.jumbotron',{
+//
+// });
+// sr.reveal('.lead.text-center');
+//
+// sr.reveal('.js1',{
+//     origin: 'left',
+// });
+// sr.reveal('.js2',{
+//     origin: 'bottom',
+// });
+// sr.reveal('.js3',{
+//     origin: 'right',
+// });
+// sr.reveal('.title');
+// sr.reveal('#projets',{
+//     origin: 'left',
+//     delay: 300,
+// });
+// sr.reveal('.row.align-items-center',{
+//     delay: 200
+// })
+// sr.reveal('.bento',{
+//     delay: 150,
+//     origin: 'right'
+// })
+// sr.reveal('.julio',{
+//     delay: 150,
+//     origin: 'left'
+// })
